07d1c6c5765eb06daad62f9d41a0eed11a8c205e
warren fung <wfung@e1z1r5p39.42.us.org>
2017-09-12 18:46:19 -0700

    updated notes

--- notes.c
+++ notes.c
@@ -1,7 +1,15 @@
 video - developing alexa skills
 //
 dynamo db
+	select basic basic w/ dynamodb setting
+	tables
+		5 read / write per sec (default)
 lambda
 dynasty
+	connects dynamo lambda and skill?
 	4/6 video, 4:10 time
 	JS, node
+
+	4/6 video
+		authentication sample for account linking
+
