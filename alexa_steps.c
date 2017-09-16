d1d807a92cf5f65f8d4a42632a1fe79931bd4228
warren fung <wfung@e1z1r8p21.42.us.org>
2017-09-15 19:19:33 -0700

    push from home please

--- alexa_steps.c
+++ alexa_steps.c
@@ -6,3 +6,11 @@ AMAZON.CancelIntent
 
 	video 6/6
 	important for publishing / submitting to amazon
+
+TESTING ALEXA SKILL
+	lambda testing - all text but is clear
+	needs more step for actual testing on device, but will be local
+
+node js
+	mocha 	for testing framework
+	chai	for insertion library
